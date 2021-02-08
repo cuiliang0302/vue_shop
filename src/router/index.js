69062270b77d7bca64bcc4bf67838fccc23c02c2
cuiliang <1554382111@qq.com>
2021-02-08 21:50:01 +0800

    数据报表开发完毕

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from "@/components/goods/Params";
 import GoodList from "@/components/goods/List"
 import GoodAdd from "@/components/goods/Add"
 import Order from "@/components/order/Order";
+import Report from "@/components/report/Report";
 // import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -28,7 +29,8 @@ const routes = [
       {path: '/params', component: Params},
       {path: '/goods', component: GoodList},
       {path: '/goods/add', component: GoodAdd},
-      {path: '/orders', component: Order}
+      {path: '/orders', component: Order},
+      {path: '/reports',component: Report}
     ]
   },
   // {
