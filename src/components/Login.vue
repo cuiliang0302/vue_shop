459d9752215ee666c3908ae5c18380e520a71313
cuiliang <1554382111@qq.com>
2021-02-09 22:54:17 +0800

    更改首页样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -32,8 +32,8 @@ export default {
     return {
       // 登录表单数据对象
       loginForm: {
-        username: 'admin',
-        password: '123456'
+        username: '',
+        password: ''
       },
       // 表单验证规则对象
       loginFormRules: {
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,15 +1,32 @@
 <template>
-<div>
-  <h3>Welcome</h3>
-</div>
+  <div>
+    <el-card>
+      <div class="flex-box">
+        <span>Welcome</span>
+        <span>欢迎登录商城后台管理系统</span>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Welcome"
+  name: "Welcome"
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.flex-box {
+  height: 500px;
+  display: flex !important;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
+.flex-box span {
+  flex: 1;
+  font-size: 30px;
+  color: #3498db;
+}
 </style>
